cea7977547c0939fa9f3d08398febb3ef8faf3f1
Chase Caster <sealgair@gmail.com>
2012-10-08 21:18:19 -0700

    fix friends list fetching logic

--- friends/models.py
+++ friends/models.py
@@ -4,21 +4,29 @@ from django.db.models import Q, Max
 
 class FriendshipManager(models.Manager):
     def friendships_for_user(self, user):
+        if user.is_anonymous():
+            return []
         return Friendship.objects.filter(Q(from_friend=user) | Q(to_friend=user))
 
     def friends_for_user(self, user, confirmed_only=False):
+        if user.is_anonymous():
+            return []
         friends = User.objects.exclude(id=user.id)
         if confirmed_only:
             friends = friends.filter(
-                invited_friends__confirmed=True,
-                invitee_friends__confirmed=True
+                Q(invited_friends__to_user=user, invited_friends__confirmed=True) |
+                Q(invitee_friends__from_user=user, invitee_friends__confirmed=True)
             )
-        friends = friends.filter(
-            Q(invited_friends__to_user=user) |
-            Q(invitee_friends__from_user=user)
-        )
+        else:
+            friends = friends.filter(
+                Q(invited_friends__to_user=user) |
+                Q(invitee_friends__from_user=user)
+            )
         for f in friends:
-            f.confirmed = f.invitee_friends.get(from_user=user).confirmed
+            try:
+                f.confirmed = f.invitee_friends.get(from_user=user).confirmed
+            except Friendship.DoesNotExist:
+                f.confirmed = f.invited_friends.get(to_user=user).confirmed
         return friends
 
 class Friendship(models.Model):
@@ -27,6 +35,12 @@ class Friendship(models.Model):
     confirmed = models.BooleanField(default=False)
 
     objects = FriendshipManager()
+
+    def __unicode__(self):
+        s = u"from {0} to {1}".format(self.from_user, self.to_user)
+        if not self.confirmed:
+            s += u" unconfirmed"
+        return s
 
     class Meta:
         unique_together = [
