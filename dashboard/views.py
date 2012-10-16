534739a77333019c43ea581e3c2d8b9003bcb39e
Chase Caster <sealgair@gmail.com>
2012-10-15 21:00:37 -0700

    drag & drop friends to make a new game

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.forms.models import inlineformset_factory
 from django.shortcuts import render_to_response, redirect
 from django.template.context import RequestContext
+
 from game.models import Player, Game
+from friends.models import Friendship
 
 def player_dashboard(request):
     """
@@ -14,13 +17,14 @@ def player_dashboard(request):
     response = None
 
     if request.method == 'POST':
-        game = Game.objects.create()
-        player = Player.objects.create(user_id=request.user.id, game=game)
-        player.judged_game_id = game.id
-        game_formset = GameInlineFormset(request.POST, instance=game)
-        if game_formset.is_valid():
-            game.save()
-            game_formset.save()
+        user_ids = request.POST.getlist('player_ids')
+        users = User.objects.filter(id__in=user_ids)
+        if users:
+            game = Game.objects.create()
+            player = Player.objects.create(user_id=request.user.id, game=game)
+            player.judged_game_id = game.id
+            for user in users:
+                Player.objects.create(user=user, game=game)
             game.save() #again, to populate the game's data with the new players
             response = redirect(reverse('game.views.game', kwargs={'game_id': game.id}))
     elif request.method == "GET":
@@ -30,7 +34,8 @@ def player_dashboard(request):
     if response is None:
         context = {
             "game_formset": game_formset,
-            "player_games": player_games
+            "player_games": player_games,
+            "friends": Friendship.objects.friends_for_user(request.user, confirmed_only=True)
         }
         response = render_to_response('dashboard/player_dashboard.html', RequestContext(request, context))
 
