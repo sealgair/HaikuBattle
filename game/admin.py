9c9d80f550191429d71243db7869a3f0d6269dd4
Chase Caster <sealgair@gmail.com>
2013-04-30 21:47:06 -0700

    Better interface to edit phrases (doing it inline in dictionary is kinda nasty)

--- game/admin.py
+++ game/admin.py
@@ -1,7 +1,11 @@
-__author__ = 'courtf'
+x__author__ = 'courtf'
 
 from django.contrib import admin
 from game.models import Dictionary, Phrase, Game, Player
+
+class PhraseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'text', 'syllables', 'dictionary']
+    list_editable = ['text', 'dictionary']
 
 class PhraseInline(admin.TabularInline):
     model = Phrase
@@ -18,5 +22,4 @@ class GameAdmin(admin.ModelAdmin):
 
 admin.site.register(Dictionary, DictionaryAdmin)
 admin.site.register(Game, GameAdmin)
-
-
+admin.site.register(Phrase, PhraseAdmin)
